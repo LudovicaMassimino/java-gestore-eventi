f173fce1d3ac787c94b7f81d077c63e32bf6b2b1
LudovicaMassimino <ludovica.massimino93@gmail.com>
2024-05-19 12:05:56 +0200

    proprietà, costruttore, getter/setter

---
+++ src/it/java/gestore/eventi/Evento.java
@@ -0,0 +1,49 @@
+package it.java.gestore.eventi;
+
+import java.time.LocalDate;
+
+public class Evento {
+	
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati;
+	
+	// costruttore
+	public Evento(String titolo, LocalDate data, int postiTotali) {
+		this.titolo = titolo;
+		this.data = data;
+		this.postiTotali = postiTotali;
+		this.postiPrenotati = 0;
+		
+	}
+	
+	// getter e setter
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(
+			String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
+	
+	
+	
+}
