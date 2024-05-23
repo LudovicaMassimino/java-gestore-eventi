58dcaeefb2e4794e6820d20a821fb7aa6682fbab
LudovicaMassimino <ludovica.massimino93@gmail.com>
2024-05-23 12:39:22 +0200

    tutto il package per testare l'app

--- src/it/java/gestore/eventi/Spettacolo.java
+++ src/it/java/gestore/eventi/Spettacolo.java
@@ -1,54 +1,5 @@
 package it.java.gestore.eventi;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+public class Spettacolo {
 
-public class Spettacolo extends Evento {
-    private LocalTime ora;
-    private double prezzo;
-
-    // costruttore
-    public Spettacolo() {
-        super("Spettacolo di Teresa Mannino - Padova", LocalDate.of(2025, 10, 13), 2500);
-        this.ora = LocalTime.of(21, 00);
-        this.prezzo = 37.00;
-    }
-
-    // getter e setter
-    public LocalTime getOra() {
-        return ora;
-    }
-
-    public void setOra(LocalTime ora) {
-        this.ora = ora;
-    }
-
-    public double getPrezzo() {
-        return prezzo;
-    }
-
-    public void setPrezzo(double prezzo) {
-        this.prezzo = prezzo;
-    }
-
-    // metodo per formattare data e ora
-    public String getDataOraFormattata() {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-
-        return getData().format(dateFormatter) + " " + ora.format(timeFormatter);
-    }
-
-    // metodo per formattare il prezzo
-    public String getPrezzoFormattato() {
-        return String.format(Locale.ITALY, "%.2f€", prezzo);
-    }
-
-    // override del metodo toString
-    @Override
-    public String toString() {
-        return getDataOraFormattata() + " - " + getTitolo() + " - " + getPrezzoFormattato();
-    }
 }
